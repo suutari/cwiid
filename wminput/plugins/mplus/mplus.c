c89e7f1ce6cb5e9f604ec321ff5855db38902f2e
Frank Löffler <knarf@cct.lsu.edu>
2013-12-15 19:49:29 -0600

    wminput/plugins/mplus/mplus.c: fix function prototype

--- wminput/plugins/mplus/mplus.c
+++ wminput/plugins/mplus/mplus.c
@@ -106,7 +106,7 @@ int wmplugin_init(int id, cwiid_wiimote_t *wiimote)
 	return 0;
 }
 
-struct wmplugin_data *wmplugin_exec(int mesg_count, union cwiid_mesg mesg[])
+struct wmplugin_data *wmplugin_exec(int mesg_count, union cwiid_mesg mesg[], struct timespec *timestamp)
 {
 	int i;
 	struct wmplugin_data *ret = NULL;
